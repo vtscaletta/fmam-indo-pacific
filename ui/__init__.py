7bc0b47ee9e23917eee457b5ef48c9a54ead78a1
vtscaletta <adkr.00447@gmail.com>
2026-06-07 21:54:33 +0500

    Import UI components and define __all__ exports

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,7 @@
+"""Слой интерфейса. Словарь строк и визуальная тема."""
 
+from ui.i18n import t, STRINGS, LANGUAGES
+from ui.theme import PALETTE, FONTS, regime_color, build_css, REGIME_COLORS
+
+__all__ = ["t", "STRINGS", "LANGUAGES", "PALETTE", "FONTS",
+           "regime_color", "build_css", "REGIME_COLORS"]
